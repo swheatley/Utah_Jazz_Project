d3e5b59747b0127bec67f7cb9ebd4f1c666ad02d
swheatley <shaylee1@me.com>
2017-02-04 23:33:12 -0700

    sticky nav created and working

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
-console.log('javascript page is working');
 
-$(document).ready(function(){
-    console.log("jquery working");
-});
+window.onload = function(){
+  const mainNav = document.querySelector(".main-nav");
+  let stickingPoint = mainNav.offsetTop;
+
+
+  function stickyNav(){
+    if(window.scrollY >= stickingPoint){
+      document.body.style.paddingTop = mainNav.offsetHeight;
+      document.body.classList.add("sticky-nav");
+      document.querySelector(".main-nav").style.position = "fixed";
+
+    }else{
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove("sticky-nav");
+      document.querySelector(".main-nav").style.position = "relative";
+    }
+  }
+
+  window.addEventListener("scroll", stickyNav);
+}
